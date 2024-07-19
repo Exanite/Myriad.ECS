8aff0e03a406514ad2be6a1a82aadb7ba2ae6217
Exanite <exanite78012@gmail.com>
2024-07-19 12:31:28 -0400

    Format

--- Myriad.ECS/NonPublicAttribute.cs
+++ Myriad.ECS/NonPublicAttribute.cs
@@ -4,7 +4,4 @@ namespace Myriad.ECS;
 /// The marked API was originally non-public, but has been made public.
 /// The API might be unsafe.
 /// </summary>
-internal class NonPublicAttribute : Attribute
-{
-
-}
+internal class NonPublicAttribute : Attribute;
